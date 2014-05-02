5fbe33d2817a51a15a46d4021c79abde0ca8781b
Brandon Riley <Ronin@192.168.1.8>
2014-05-02 10:00:19 -0700

    added a test class

---
+++ CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Brandon Riley on 5/2/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Brandon Riley on 5/2/14.
+//
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
